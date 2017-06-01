13606a84d2a3b24af24d001e1cfb432527461e75
Dima Fedotov <crismsfiva@gmail.com>
2017-06-01 17:16:16 +0300

    Фикс формы

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json");
 $name = $_POST["name"];
 $message = "Сообщение от пользователя: $name";
 
-$result = mail("dimanmw2@yandex.ru", "тест письма", $message);
+$result = mail("crismsfiva@gmail.com", "тест письма", $message);
 
 
 echo json_encode(array('status' => $result));
